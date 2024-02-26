7b1fafb95895527b81cae07ae2ec18bbb49684a8
thomas <thomas.bataille@solveg.fr>
2024-02-26 22:33:59 +0100

    paiment ok

--- paiment.php
+++ paiment.php
@@ -1,17 +1,19 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
 	<title>Webleb</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="Front/css/style-payment.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body>
 	<div class="modal">
-		<form class="form">
+		<form class="form" id="payment-form">
 			<div class="paymentother">
-				<button name="apple-pay" type="button">
-					<svg xml:space="preserve" viewBox="0 0 512 210.2" y="0px" x="0px" id="Layer_1" version="1.1"
+				</button>
+				<button name="google-pay" type="button">
+				<svg xml:space="preserve" viewBox="0 0 512 210.2" y="0px" x="0px" id="Layer_1" version="1.1"
 						xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">
 						<path
 							d="M93.6,27.1C87.6,34.2,78,39.8,68.4,39c-1.2-9.6,3.5-19.8,9-26.1c6-7.3,16.5-12.5,25-12.9  C103.4,10,99.5,19.8,93.6,27.1 M102.3,40.9c-13.9-0.8-25.8,7.9-32.4,7.9c-6.7,0-16.8-7.5-27.8-7.3c-14.3,0.2-27.6,8.3-34.9,21.2  c-15,25.8-3.9,64,10.6,85c7.1,10.4,15.6,21.8,26.8,21.4c10.6-0.4,14.8-6.9,27.6-6.9c12.9,0,16.6,6.9,27.8,6.7  c11.6-0.2,18.9-10.4,26-20.8c8.1-11.8,11.4-23.3,11.6-23.9c-0.2-0.2-22.4-8.7-22.6-34.3c-0.2-21.4,17.5-31.6,18.3-32.2  C123.3,42.9,107.7,41.3,102.3,40.9 M182.6,11.9v155.9h24.2v-53.3h33.5c30.6,0,52.1-21,52.1-51.4c0-30.4-21.1-51.2-51.3-51.2H182.6z   M206.8,32.3h27.9c21,0,33,11.2,33,30.9c0,19.7-12,31-33.1,31h-27.8V32.3z M336.6,169c15.2,0,29.3-7.7,35.7-19.9h0.5v18.7h22.4V90.2  c0-22.5-18-37-45.7-37c-25.7,0-44.7,14.7-45.4,34.9h21.8c1.8-9.6,10.7-15.9,22.9-15.9c14.8,0,23.1,6.9,23.1,19.6v8.6l-30.2,1.8  c-28.1,1.7-43.3,13.2-43.3,33.2C298.4,155.6,314.1,169,336.6,169z M343.1,150.5c-12.9,0-21.1-6.2-21.1-15.7c0-9.8,7.9-15.5,23-16.4  l26.9-1.7v8.8C371.9,140.1,359.5,150.5,343.1,150.5z M425.1,210.2c23.6,0,34.7-9,44.4-36.3L512,54.7h-24.6l-28.5,92.1h-0.5  l-28.5-92.1h-25.3l41,113.5l-2.2,6.9c-3.7,11.7-9.7,16.2-20.4,16.2c-1.9,0-5.6-0.2-7.1-0.4v18.7C417.3,210,423.3,210.2,425.1,210.2z"
@@ -59,24 +61,43 @@
 			<div class="card-info">
 				<div class="input_container">
 					<label class="input_label">Card holder name</label>
-					<input class="input_field" type="text" placeholder="Enter your full name">
+					<input class="input_field" type="text" placeholder="Enter your full name" required>
 				</div>
 				<div class="input_container">
 					<label class="input_label">Card Number</label>
-					<input class="input_field" type="number" placeholder="0000 0000 0000 0000">
+					<input class="input_field" type="number" placeholder="0000 0000 0000 0000" required>
 				</div>
 				<div class="input_container">
 					<label class="input_label">Expiry Date / CVV</label>
 					<div class="split">
-						<input class="input_field" style="width: 250px;" type="text" placeholder="01/23">
-						<input class="input_field" style="width: 110px;" type="number" placeholder="CVV">
+						<input class="input_field" style="width: 250px;" type="text" placeholder="01/23" required>
+						<input class="input_field" style="width: 110px;" type="number" placeholder="CVV" required>
 					</div>
 				</div>
 			</div>
-			<button class="checkout" style="cursor: pointer;">
+			<button class="checkout" type="submit">
 				<span>Checkout</span>
 			</button>
 		</form>
 	</div>
+
+	<script>
+		$(document).ready(function () {
+			$('#payment-form').submit(function (e) {
+				e.preventDefault(); // Prevent form submission
+
+				// Perform form validation here
+				var isValid = true; // Implement your validation logic
+
+				if (isValid) {
+					// Redirect to home.php
+					window.location.href = 'home.php';
+
+					// Show payment success notification
+					alert('Payment successful! Thank you for your trust.');
+				}
+			});
+		});
+	</script>
 </body>
 </html>
