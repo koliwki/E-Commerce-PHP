e3cbd9eeedd9abfaaa4c6f97230e900d9b192510
thomas <thomas.bataille@solveg.fr>
2024-02-26 23:36:54 +0100

    db avec com etc

--- e_commerce.sql
+++ e_commerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 26 fév. 2024 à 03:55
+-- Généré le : lun. 26 fév. 2024 à 23:35
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -26,7 +26,6 @@ SET time_zone = "+00:00";
 --
 -- Structure de la table `address`
 --
-DROP TABLE IF EXISTS `address`;
 
 CREATE TABLE `address` (
   `address_id` int(11) NOT NULL,
@@ -42,7 +41,6 @@ CREATE TABLE `address` (
 --
 -- Structure de la table `cart`
 --
-DROP TABLE IF EXISTS `cart`;
 
 CREATE TABLE `cart` (
   `cart_id` int(11) NOT NULL,
@@ -57,14 +55,16 @@ CREATE TABLE `cart` (
 --
 
 INSERT INTO `cart` (`cart_id`, `user_id`, `product_id`, `quantity`, `added_date`) VALUES
-(29, 2, 8, 1, '2024-02-26 02:38:21');
+(86, 9, 10, 1, '2024-02-26 21:42:57'),
+(87, 9, 8, 3, '2024-02-26 21:43:08'),
+(88, 7, 10, 3, '2024-02-26 21:52:10'),
+(89, 7, 7, 1, '2024-02-26 21:54:27');
 
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `command`
 --
-DROP TABLE IF EXISTS `command`;
 
 CREATE TABLE `command` (
   `command_id` int(11) NOT NULL,
@@ -79,9 +79,43 @@ CREATE TABLE `command` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `comments`
+--
+
+CREATE TABLE `comments` (
+  `comment_id` int(11) NOT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `comments`
+--
+
+INSERT INTO `comments` (`comment_id`, `product_id`, `user_id`, `content`, `created_at`) VALUES
+(1, 7, NULL, 'jaime biennnn', '2024-02-26 22:02:38'),
+(2, 7, NULL, 'jaime biennnn', '2024-02-26 22:04:24'),
+(3, 7, NULL, 'jaime biennnn', '2024-02-26 22:04:57'),
+(4, 7, NULL, 'lourd \r\n', '2024-02-26 22:05:05'),
+(5, 7, NULL, 'jaime biennnn', '2024-02-26 22:06:34'),
+(6, 7, 7, 'ok', '2024-02-26 22:29:02'),
+(7, 7, 7, 'cest vraiment du lourd wow ', '2024-02-26 22:30:08'),
+(8, 2, 7, 'ca glisse comme brice de nice ', '2024-02-26 22:30:29'),
+(9, 2, 9, 'jai glisse jusquen laponie ', '2024-02-26 22:30:57'),
+(10, 7, 9, 'jai pas reussi a les portes cest lourd ', '2024-02-26 22:31:21'),
+(11, 10, 9, 'ca rebondit bien \r\n', '2024-02-26 22:31:51'),
+(12, 3, 9, 'hmmm delicious ', '2024-02-26 22:32:01'),
+(13, 3, 7, 'jaime trop les kinder ', '2024-02-26 22:32:42'),
+(14, 9, 7, 'mon chien et moi on aime beaucoup', '2024-02-26 22:33:05'),
+(15, 8, 7, 'ca facile le transit ', '2024-02-26 22:33:21');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `invoices`
 --
-DROP TABLE IF EXISTS `invoices`;
 
 CREATE TABLE `invoices` (
   `invoice_id` int(11) NOT NULL,
@@ -98,7 +132,6 @@ CREATE TABLE `invoices` (
 --
 -- Structure de la table `product`
 --
-DROP TABLE IF EXISTS `product`;
 
 CREATE TABLE `product` (
   `product_id` int(11) NOT NULL,
@@ -117,14 +150,15 @@ INSERT INTO `product` (`product_id`, `product_name`, `description`, `price`, `ca
 (2, 'planche de surf ', 'vend planche de surf neuve ', 499, 'sport', 'https://www.surfmarket.org/images/stories/virtuemart/product/dhd-twin-fin-blue-scarlett.jpg'),
 (3, 'boite de chocolat', 'boite de chocolat assortie avec plein de surprise ', 50, 'nourriture', 'https://www.mesechantillonsgratuits.fr/wp-content/uploads/2021/03/assortiment-de-barre-au-chocolat-20-pieces-2048x1676.png'),
 (7, 'altere', 'cest du lourd', 55, 'sport', 'https://i.pinimg.com/originals/aa/24/ef/aa24efdb8ca97c8889604dd45a5fefed.jpg'),
-(8, 'cable ethernet ', 'cable ethernet haute performances ', 15, 'autres', 'https://th.bing.com/th/id/OIP.2AV9NvFzgaPc636VES9BOwAAAA?rs=1&pid=ImgDetMain');
+(8, 'cable ethernet ', 'cable ethernet haute performances ', 15, 'autres', 'https://th.bing.com/th/id/OIP.2AV9NvFzgaPc636VES9BOwAAAA?rs=1&pid=ImgDetMain'),
+(9, 'croquette pour chien', 'croquette pour chien', 20, 'autres', 'https://images-na.ssl-images-amazon.com/images/I/81dCdPMezkL._AC_SL1500_.jpg'),
+(10, 'balle de tenis', 'plusie', 40, 'sport', 'https://www.ecotennis.ca/wp-content/uploads/2018/06/00100dPORTRAIT_00100_BURST20180619073630606_COVER-1080x675.jpg');
 
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `user`
 --
-DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
   `username` varchar(255) NOT NULL,
@@ -142,9 +176,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`username`, `password`, `email`, `firstname`, `lastname`, `country`, `phone_number`, `user_id`) VALUES
-('admin', 'azer', 'admin@admin.com', '', '', '', NULL, 1),
-('akaza', 'az', 'az@az', '', '', '', NULL, 2),
-('a', 'a', 'a@a.a', '', '', '', NULL, 5);
+('koliwki', '$2y$10$uZQ9UWb1s20UjICjklfCPuhia/s19Ru./tjt7teMaX74hUH/ZSCAW', 'thomas.bataille@solveg.fr', '', '', '', NULL, 7),
+('admin', '$2y$10$eOcZi93VeTGBDghDpDjazORljqFj.JjnhIlOtixygNEa2PDbq1kE.', 'admin@admin.com', '', '', '', NULL, 8),
+('akaza', '$2y$10$lZYtfYBYcf94fKitLiosIeZHpFsfxQbjSjn5ortLRccXGP55HKxVO', 'az@az', 'Thomas', 'Bataille', 'France', 624574408, 9);
 
 --
 -- Index pour les tables déchargées
@@ -157,6 +191,12 @@ ALTER TABLE `cart`
   ADD PRIMARY KEY (`cart_id`),
   ADD UNIQUE KEY `unique_cart_product` (`user_id`,`product_id`),
   ADD KEY `product_id` (`product_id`);
+
+--
+-- Index pour la table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`);
 
 --
 -- Index pour la table `product`
@@ -178,19 +218,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
+
+--
+-- AUTO_INCREMENT pour la table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT pour la table `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Contraintes pour les tables déchargées
